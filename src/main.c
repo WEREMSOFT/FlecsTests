963929ed7700f7a0e26d14759fc901528740eec3
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-10-28 12:26:02 +0100

    Implemented scene graph and asked for help

--- src/main.c
+++ src/main.c
@@ -1,121 +1,128 @@
-#include <stdio.h>
-#include "flecs.h"
+#include <flecs.h>
 #include "tigr.h"
-#include <math.h>
 
-#define MATRIX_SIZE_WIDTH 200
-#define MATRIX_SIZE_HEIGHT 200
+Tigr *screen, *image;
 
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT  600
-
-#define SCREEN_CENTER_X SCREEN_WIDTH / 2
-#define SCREEN_CENTER_Y SCREEN_HEIGHT / 2
-
-typedef float PositionX;
-typedef float PositionY;
-typedef float PositionZ;
-
-typedef struct Position {
+/* Component types */
+typedef struct Vector2D {
     float x;
     float y;
-    float z;
-} Position;
+} Vector2D;
 
-typedef int32_t Speed;
+typedef Vector2D Position;
+typedef Vector2D WorldPosition;
+typedef Vector2D Velocity;
 
-Tigr *screen;
+/* Implement a simple move system */
+void Move(ecs_rows_t *rows) {
+    /* Get the two columns from the system signature */
+    ECS_COLUMN(rows, Position, p, 1);
+    ECS_COLUMN(rows, Velocity, v, 2);
+
+    printf("moving\n");
+    for (int i = 0; i < rows->count; i ++) {
+        p[i].x += v[i].x * rows->delta_time;
+        p[i].y += v[i].y * rows->delta_time;
+    }
+}
+
+/* Implement a system that transforms world coordinates hierarchically. If the 
+ * CASCADE column is set, it points to the world coordinate of the parent. This
+ * will be used to then transform Position to WorldPosition of the child.
+ * If the CASCADE column is not set, the system matched a root. In that case,
+ * just assign the Position to the WorldPosition. */
+void Transform(ecs_rows_t *rows) {
+    /* Get the two columns from the system signature */
+    ECS_COLUMN(rows, WorldPosition, parent_wp, 1);
+    ECS_COLUMN(rows, WorldPosition, wp, 2);
+    ECS_COLUMN(rows, Position, p, 3);
+
+    if (!parent_wp) {
+        printf("transforming parent\n");
+        for (int i = 0; i < rows->count; i ++) {
+            wp[i].x = p[i].x;
+            wp[i].y = p[i].y;
+        }
+    } else {
+        printf("transforming childs\n");
+        for (int i = 0; i < rows->count; i ++) {
+            wp[i].x = parent_wp->x + p[i].x;
+            wp[i].y = parent_wp->y + p[i].y;
+        }
+    }
+}
 
 void Render(ecs_rows_t *rows) {
-    ECS_COLUMN(rows, PositionX, x, 1);
-    ECS_COLUMN(rows, PositionY, y, 2);
-    ECS_COLUMN(rows, PositionZ, z, 3);
-    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
-    TPixel px = { 0xff, 0xff, 0xff, 0xff };
+    ECS_COLUMN(rows, WorldPosition, p, 1);
+
     for (int i = 0; i < rows->count; i ++) {
-        tigrPlot(screen, x[i] * z[i], y[i] * z[i], px);
+        tigrBlitAlpha(screen, image, p[i].x, p[i].y, 0, 0, image->w, image->h, 1);
     }
+    printf("Rendering.\n");
+}
+
+void UpdateScreen() {
     tigrUpdate(screen);
-}
-
-float distance(float x, float y) {
-    return sqrtf(powf(SCREEN_CENTER_X - x, 2) + powf(SCREEN_CENTER_Y - y, 2));
-}
-
-void UpdateParticle(ecs_rows_t *rows){
-    ECS_COLUMN(rows, PositionX, x, 1);
-    ECS_COLUMN(rows, PositionY, y, 2);
-
-    static float phase = 0;
-    phase += 10 * rows->delta_time;
-    const int paddingX = 2;
-    const int paddingY = 2;
-    const int offsetX = (SCREEN_WIDTH - MATRIX_SIZE_WIDTH * paddingX) / 2;
-    const int offsetY = (SCREEN_HEIGHT - MATRIX_SIZE_HEIGHT * paddingY) / 2;
-
-    float phaseOffset = 0;
-
-
-    for(int col = 0; col < MATRIX_SIZE_WIDTH; col++){
-        for(int row = 0; row < MATRIX_SIZE_HEIGHT; row++){
-//            phaseOffset += .3;
-            phaseOffset = distance(x[col * MATRIX_SIZE_HEIGHT + row], y[col * MATRIX_SIZE_HEIGHT + row]) * .1;
-            x[col * MATRIX_SIZE_HEIGHT + row] = col * paddingX + offsetX + sin(phase + phaseOffset) * 5;
-            y[col * MATRIX_SIZE_HEIGHT + row] = row * paddingY + offsetY + cos(phase + phaseOffset) * 5;
-        }
-    }
-
-}
-
-void InitMatrix(ecs_rows_t *rows){
-    ECS_COLUMN(rows, PositionX, x, 1);
-    ECS_COLUMN(rows, PositionY, y, 2);
-    ECS_COLUMN(rows, PositionZ, z, 3);
-
-    const int paddingX = 10;
-    const int paddingY = 10;
-    const int offsetX = (SCREEN_WIDTH - MATRIX_SIZE_WIDTH * paddingX) / 2;
-    const int offsetY = (SCREEN_HEIGHT - MATRIX_SIZE_HEIGHT * paddingY) / 2;
-
-    float zDistance = 1.0;
-
-    for(int col = 0; col < MATRIX_SIZE_WIDTH; col++){
-        for(int row = 0; row < MATRIX_SIZE_HEIGHT; row++){
-            x[col * MATRIX_SIZE_HEIGHT + row] = col * paddingX + offsetX;
-            y[col * MATRIX_SIZE_HEIGHT + row] = row * paddingY + offsetY;
-            z[col * MATRIX_SIZE_HEIGHT + row] = zDistance;
-        }
-    }
-
+    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+    printf("Updating screen\n");
 }
 
 int main(int argc, char *argv[]) {
+    /* Create the world, pass arguments for overriding the number of threads,fps
+     * or for starting the admin dashboard (see flecs.h for details). */
     ecs_world_t *world = ecs_init_w_args(argc, argv);
 
-    /* Register components and systems */
-    ECS_COMPONENT(world, PositionX);
-    ECS_COMPONENT(world, PositionY);
-    ECS_COMPONENT(world, PositionZ);
+    screen = tigrWindow(640, 480, "Hierarchy test", TIGR_2X);
+    image = tigrLoadImage("assets/squinkle.png");
 
-    ECS_SYSTEM(world, Render, EcsOnUpdate, PositionX, PositionY, PositionZ);
+    /* Register components */
+    ECS_COMPONENT(world, Position);
+    ECS_COMPONENT(world, WorldPosition);
+    ECS_COMPONENT(world, Velocity);
 
-    ECS_SYSTEM(world, UpdateParticle, EcsOnUpdate, PositionX, PositionY);
+    /* Move entities with Position and Velocity */
+    ECS_SYSTEM(world, Move, EcsOnUpdate, Position, Velocity);
 
-    ECS_SYSTEM(world, InitMatrix, EcsOnAdd, PositionX, PositionY, PositionZ);
+    /* Transform local coordinates to world coordinates. A CASCADE column
+     * guarantees that entities are evaluated breadth-first, according to the
+     * hierarchy. This system will depth-sort based on parents that have the
+     * WorldPosition component. */
+    ECS_SYSTEM(world, Transform, EcsOnUpdate, CASCADE.WorldPosition, WorldPosition, Position);
+    ECS_SYSTEM(world, Render, EcsOnUpdate, WorldPosition);
 
-    ECS_TYPE(world, DotMatrix, PositionX, PositionY, PositionZ);
+    /* Create root of the hierachy which moves around */
+    ECS_ENTITY(world, Root, WorldPosition, Position, Velocity);
+    ecs_set(world, Root, Position, {0, 0});
+    ecs_set(world, Root, Velocity, {10, 5});
 
-    ecs_entity_t e = ecs_new_w_count(world, DotMatrix, MATRIX_SIZE_WIDTH * MATRIX_SIZE_HEIGHT);
+    /* Create children that don't move and are relative to the parent */
+    ECS_ENTITY(world, Child1, WorldPosition, Position, CHILDOF | Root);
+    ecs_set(world, Child1, Position, {100, 100});
 
-    /* Limit application to 60 FPS */
-    ecs_set_target_fps(world, 60);
+    ECS_ENTITY(world, GChild1, WorldPosition, Position, CHILDOF | Child1);
+    ecs_set(world, GChild1, Position, {10, 10});
 
-    screen = tigrWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello", TIGR_2X);
+    ECS_ENTITY(world, Child2, WorldPosition, Position, CHILDOF | Root);
+    ecs_set(world, Child2, Position, {20, 20});
 
-    /* Progress world in main loop (invokes Move system) */
-    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
-        ecs_progress(world, 0);
-    }
+    ECS_ENTITY(world, GChild2, WorldPosition, Position, CHILDOF | Child2);
+    ecs_set(world, GChild2, Position, {200, 200});
+
+    /* Set target FPS for main loop to 1 frame per second */
+    ecs_set_target_fps(world, 1);
+
+    printf("Application move_system is running, press CTRL-C to exit...\n");
+    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+
+    ECS_SYSTEM(world, UpdateScreen, EcsPostUpdate, 0);
+
+    /* Run systems */
+    while (!tigrKeyDown(screen, TK_ESCAPE) && ecs_progress(world, 0)){
+        printf("######### mainLoop Called #######\n");
+    };
+
     tigrFree(screen);
+    tigrFree(image);
+    /* Cleanup */
     return ecs_fini(world);
 }
